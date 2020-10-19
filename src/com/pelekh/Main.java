1f5fe8e1a1ef13e57a8a97fbe22e84eaf943b50e
Khrystyna Pelekh <Khrystyna.Pelekh@outlook.com>
2020-10-20 01:15:42 +0300

    Add output of address and name

--- src/com/pelekh/Main.java
+++ src/com/pelekh/Main.java
@@ -1,8 +1,17 @@
 package com.pelekh;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("What is your name? ");
+        String name = bufferedReader.readLine();
+        System.out.print("Where are you live, " + name + "? ");
+        String address = bufferedReader.readLine();
+        System.out.println("Your name: " + name + ";\nYour address: " + address + ".");
     }
 }
