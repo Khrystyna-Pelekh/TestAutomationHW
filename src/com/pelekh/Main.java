bacc03656ad1f82a8a31e92a5b7ee89d3d3adbaa
Khrystyna Pelekh <Khrystyna.Pelekh@outlook.com>
2020-10-22 02:33:45 +0300

    Edit replace functionality. Add messages in print method.
    Add a check if length of number is more than 1

--- src/com/pelekh/Main.java
+++ src/com/pelekh/Main.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.print("Enter a natural number: ");
         Integer number = null;
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         try {
@@ -15,13 +16,13 @@ public class Main {
         }
         String n2 = String.valueOf(number * number);
         System.out.println(n2.contains("3") ? "Number n^2 contains digit 3" : "Number n^2 doesn't contain digit 3");
-        System.out.println(new StringBuilder(number.toString()).reverse());
+        System.out.println("Reverse: " + new StringBuilder(number.toString()).reverse());
 
         String replace = number.toString();
-        replace = replace.charAt(replace.length() - 1) + replace.substring(1, replace.length() - 1)
-                + number.toString().charAt(0);
-        System.out.println(replace);
+        replace = replace.length() > 1 ? replace.charAt(replace.length() - 1) + replace.substring(1, replace.length() - 1)
+                + number.toString().charAt(0) : replace;
+        System.out.println("Replace the first with last: " + replace);
 
-        System.out.println("1" + number.toString() + "1");
+        System.out.println("Write 1 in begin and end: 1" + number.toString() + "1");
     }
 }
